ca6497adc86b79543a7bda4dcf712a577bfd0c25
andy <andylouisjean@gmail.com>
2020-03-23 02:20:58 -0400

    commit pot hole project

---
+++ script/addNew.js
@@ -0,0 +1,13 @@
+
+function loginbutton(){
+    let userlogin = prompt("Enter your login please")
+    return
+}
+logibbutton();
+
+function addNewPot() {
+    let userinput = prompt("Add new Pot hole name")
+    document.getElementById("demo").innerHTML = "";
+  }
+
+  addNewPot();
